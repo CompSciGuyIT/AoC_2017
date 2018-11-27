dad30fbdd231645f45dcbec3da1b2fc2c34b21ec
Gary Murphy <compsciguy.it@gmail.com>
2018-11-27 17:57:29 +1000

    Completed Day 5 Part 2

---
+++ Day_5/TwistyTrampolines_Part2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main (void) {
+    int array[1003];    // 1003 is the number of integers in the file
+    int steps = 0;
+    int index = 0;
+
+    // Read file into array
+    ifstream file("input.txt");
+    string line;
+    int value;
+
+    while(getline(file, line)) {
+        stringstream integer(line);
+        integer >> value;
+
+        array[index] = value;
+
+        index++;
+    }
+
+    index = 0;
+
+    // Loop until index is out of bounds
+    while(1) {
+        // Check value of Array at index
+        if (array[index] > 2) {
+            // Decrement value of Array at index
+            array[index]--;
+
+            // Add value to index plus 1
+            index += (array[index] + 1);
+        } else {
+            // Increment value of Array at index
+            array[index]++;
+
+            // Add value to index minus 1
+            index += (array[index] - 1);
+        }
+
+        // Increment steps
+        steps++;
+
+        // Check if index is out of bounds
+        if (index < 0 || index > 1002) {
+            cout << "Steps: " << steps << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
